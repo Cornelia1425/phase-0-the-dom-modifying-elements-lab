3422a8543e5c17251b4add36aa440ca8087f7267
yiranshu <shuyiran1425@hotmail.com>
2024-03-08 19:22:31 -0500

    champion

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Yiran is the champion";
+
+/*
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Yiran is the champion";
+*/
